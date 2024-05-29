39d69cfc973ba862c0ec47a1c036fb5dcb54443c
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2024-05-30 11:07:50 +1200

    I made a function to get mouse coords

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('Canvas');
 const ctx = canvas.getContext('2d');
+
+canvas.addEventListener('mousemove', function(event) {
+    console.log('Mouse coordinates on page:', event.clientX, event.clientY);
+});
 
 let cannon = {
     x: canvas.width / 2,
@@ -8,4 +12,15 @@ let cannon = {
     height: 100,
 };
 
+function drawCannon() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(cannon.x - cannon.width / 2, cannon.y - cannon.height / 2, cannon.width, cannon.height);
+}
 
+function loop() {
+    drawCannon();
+    requestAnimationFrame(loop);
+}
+
+loop();
