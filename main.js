34b6f6ba85086332cb459bd1f9a4eee11979239c
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2024-05-29 15:01:00 +1200

    tried to get the cannon to point to sursor but failed

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+let cannon = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    width: 25,
+    height: 100,
+};
+
+
